f84141bc1f9fda1e229f69f6ca97ecfe98f4718c
ernest <shiwb@shideMac-mini.local>
2024-02-18 17:19:02 +0800

    ernest: support local bridge node

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,8 +6,13 @@ if [ "$1" = 'celestia-da' ]; then
     echo "Initializing Celestia Node with command:"
 
     if [[ -n "$NODE_STORE" ]]; then
+      if [ "$NODE_TYPE" = "brdige" ]; then
+        echo "celestia-da "${NODE_TYPE}" init --node.store "${NODE_STORE}""
+        celestia-da "${NODE_TYPE}" init --node.store "${NODE_STORE}"
+      else
         echo "celestia-da "${NODE_TYPE}" init --p2p.network "${P2P_NETWORK}" --node.store "${NODE_STORE}""
         celestia-da "${NODE_TYPE}" init --p2p.network "${P2P_NETWORK}" --node.store "${NODE_STORE}"
+      fi
     else
         echo "celestia-da "${NODE_TYPE}" init --p2p.network "${P2P_NETWORK}""
         celestia-da "${NODE_TYPE}" init --p2p.network "${P2P_NETWORK}"
