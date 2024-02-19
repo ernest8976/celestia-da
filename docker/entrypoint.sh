98f3b0fe07c3e5d6ab78abfd8a252d0198c826dc
ernest <shiwb@shideMac-mini.local>
2024-02-19 16:08:54 +0800

    ernest: support private network

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,12 +6,12 @@ if [ "$1" = 'celestia-da' ]; then
     echo "Initializing Celestia Node with command:"
 
     if [[ -n "$NODE_STORE" ]]; then
-      if [ "$NODE_TYPE" = "brdige" ]; then
-        echo "celestia-da "${NODE_TYPE}" init --node.store "${NODE_STORE}""
-        celestia-da "${NODE_TYPE}" init --node.store "${NODE_STORE}"
-      else
+      if [ -n "$P2P_NETWORK" ]; then
         echo "celestia-da "${NODE_TYPE}" init --p2p.network "${P2P_NETWORK}" --node.store "${NODE_STORE}""
         celestia-da "${NODE_TYPE}" init --p2p.network "${P2P_NETWORK}" --node.store "${NODE_STORE}"
+      else
+        echo "celestia-da "${NODE_TYPE}" init --node.store "${NODE_STORE}""
+        celestia-da "${NODE_TYPE}" init --node.store "${NODE_STORE}"
       fi
     else
         echo "celestia-da "${NODE_TYPE}" init --p2p.network "${P2P_NETWORK}""
